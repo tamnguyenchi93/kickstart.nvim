84c2b55d8fd8e7169383cfb4653bcb8250c6aa9a
Tam Nguyen <tamnguyenchi93>
2024-08-09 09:48:33 +0000

    init: add vimbegood

--- init.lua
+++ init.lua
@@ -885,7 +885,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.vimbegood', -- adds vimbegood game
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
