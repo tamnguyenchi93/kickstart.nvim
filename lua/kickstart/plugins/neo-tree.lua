631414b78d8d9050115c24a68a15d612ed9faa07
Tam Nguyen <tamnguyenchi93>
2024-08-09 09:45:40 +0000

    neotree: show hidden files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,18 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
